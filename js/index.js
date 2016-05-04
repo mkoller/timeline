a3fd34b3d38acfd3f2681e13a20d6b3a19cf3371
Miles <mileskoller@gmail.com>
2016-05-04 15:14:24 -0500

    theme updates

--- js/index.js
+++ js/index.js
@@ -2,11 +2,15 @@
 new WOW().init();
 
 //faq toggle stuff 
-$('.togglefaq').click(function(e) {
+$('.expand-more').click(function(e) {
 e.preventDefault();
+
 var notthis = $('.active').not(this);
+
 notthis.find('.icon-minus').addClass('icon-plus').removeClass('icon-minus');
-notthis.toggleClass('active').next('.faqanswer').slideToggle(300);
- $(this).toggleClass('active').next().slideToggle("fast");
+notthis.toggleClass('active').next('.expand-less').slideToggle(300);
+ 
+$(this).toggleClass('active').next().slideToggle("fast");
 $(this).children('i').toggleClass('icon-plus icon-minus');
+
 });
